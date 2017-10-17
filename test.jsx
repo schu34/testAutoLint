00f6a34ee7406de2a66b7b3bbbcb1f595253cbfb
Matthew Schupack <schu34@gmail.com>
2017-10-17 15:18:41 -0400

    add jsx error

--- test.jsx
+++ test.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ChatJSON extends Component{
 	render(){
 		return (
-      <div>
+      <div onClick={()=>1}>
         This is the Chat JSON view!
       </div>
 		)
