d0a49c0b826f416ffb1173d064f67294a2ad5ab4
Matthew Schupack <schu34@gmail.com>
2017-10-17 15:17:48 -0400

    jsx to test

---
+++ test.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ChatJSON extends Component{
+	render(){
+		return (
+      <div>
+        This is the Chat JSON view!
+      </div>
+		)
+	}
+}
+
+export default ChatJSON;
