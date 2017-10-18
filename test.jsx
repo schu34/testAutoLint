847b51a8f59f6ec7e526a393c8ea4db9c6576a3a
Matthew Schupack <schu34@gmail.com>
2017-10-18 10:00:55 -0400

    fix errors

--- test.jsx
+++ test.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ChatJSON extends Component{
 	render(){
 		return (
-      <div onClick={()=>1}>
+      <div>
         This is the Chat JSON view!
       </div>
 		)
